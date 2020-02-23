f2704ebdbf26112846f90135bbbfb7defde9e94b
mobscene <s_aint@list.ru>
2020-02-23 14:17:19 +0600

    add registration page

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,18 +1,110 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>registration</h1>
-            </v-flex>
-        </v-layout>
-        
-    </v-container>
+    <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="5"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Registration form</v-toolbar-title>
+                
+                
+              </v-toolbar>
+              <v-card-text>
+                <v-form 
+                ref="form"
+                v-model="valid"
+                validation>
+                  <v-text-field
+                    label="Email"
+                    name="email"
+                    prepend-icon="mdi-account"
+                    type="text"
+                    v-model="email"
+                    :rules="emailRules"
+                  />
+
+                  <v-text-field
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                    v-model="password"
+                    :counter="6"
+                    :rules="passwordRules"
+                  />
+
+                  <v-text-field
+                    label="Confirm Password"
+                    name="confirm-password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                    v-model="confirmPassword"
+                    :counter="6"
+                    :rules="confirmPasswordRules"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn 
+                color="primary"
+                @click="onSubmit"
+                :disabled="!valid"
+                >Create account</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
 </template>
 
 <script>
 export default {
     data (){
-        return{}
+        return{
+          email: '',
+          password: '',
+          confirmPassword: '',
+          valid: false,
+          emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+          ],
+          passwordRules: [
+            v => !!v || 'Password is required',
+            v => (v && v.length >= 6) || 'Password must be less than 6 characters',
+          ],
+          confirmPasswordRules: [
+              v => !!v || 'Password is required',
+              v => v === this.password || 'Password should match'
+          ]
+        }
+    },
+    methods: {
+      onSubmit () {
+          if (this.$refs.form.validate()){
+            const user = {
+              email: this.email,
+              password: this.password
+            }
+
+            // eslint-disable-next-line no-console
+            console.log(user)
+          }
+      }
     }
 }
 </script>
